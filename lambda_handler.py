06bd12a6a4ebf8582e01f732c2a63b1ad5235f31
Unmesh Reza <unmesh17780@gmail.com>
2026-03-17 14:03:56 -0400

    Revert "Add AWS Lambda deployment with Mangum"
    This reverts commit 4a2024b2b7ab8c10fe4d970de3ae08f6e1bf24b1.

--- lambda_handler.py
+++
@@ -1,7 +0,0 @@
-"""AWS Lambda handler for Inside Imaging Flask app using Mangum."""
-
-from mangum import Mangum
-from app import app
-
-# Wrap the Flask app with Mangum to make it compatible with AWS Lambda
-handler = Mangum(app, lifespan="off")
